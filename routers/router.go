91bf38d6e790a02566bbdae6eb3e923c864113ae
qiuapeng921 <qiuapeng921@163.com>
2020-04-05 16:01:44 +0800

    update

--- routers/router.go
+++ routers/router.go
@@ -4,6 +4,7 @@ import (
 	"gin-blog/app/controller"
 	"gin-blog/app/middleware"
 	"gin-blog/app/socket"
+	"gin-blog/helpers/response"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,4 +13,10 @@ func SetupRouter(router *gin.Engine) {
 
 	router.GET("/", controller.Index)
 	router.GET("/ws", socket.WsHandler)
+
+	router.NoRoute(func(context *gin.Context) {
+		response.Context(context).View("index", gin.H{
+			"name": "GinFrame",
+		})
+	})
 }
