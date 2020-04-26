83aed8e5d1bef6d7c637eefcc16738118f7472a8
qiuapeng921 <qiuapeng921@163.com>
2020-04-26 22:43:40 +0800

    update

--- helpers/pool/gredis/redis.go
+++ helpers/pool/gredis/redis.go
@@ -1,43 +1,52 @@
 package gredis
 
 import (
+	"fmt"
+	"github.com/go-redis/redis"
 	"os"
 	"strconv"
+	"sync"
 	"time"
-
-	"github.com/gomodule/redigo/redis"
 )
 
-type Redis struct {
-	conn *redis.Pool
+type RedisSingleton struct {
+	client *redis.Client
 }
 
-func SetupRedis() {
+var (
+	once           sync.Once
+	redisSingleton *RedisSingleton
+	client         *redis.Client
+)
+
+func SetupRedis() *redis.Client {
+	Address := os.Getenv("REDIS_HOST")
+	Password := os.Getenv("REDIS_PASSWORD")
 	Idle, _ := strconv.Atoi(os.Getenv("MAX_IDLE"))
 	Active, _ := strconv.Atoi(os.Getenv("MAX_ACTIVE"))
-	var RedisConn Redis
-	RedisConn.conn = &redis.Pool{
-		MaxIdle:     Idle,   //最大空闲数
-		MaxActive:   Active, //最大连接数据库连接数
-		IdleTimeout: 200,    // 超时
-		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", os.Getenv("REDIS_HOST"))
-			if err != nil {
-				panic(err.Error())
-			}
-			return c, err
-		},
-		TestOnBorrow: func(c redis.Conn, t time.Time) error {
-			_, err := c.Do("PING")
-			panic(err.Error())
-		},
+	client = redis.NewClient(&redis.Options{
+		Addr:        Address,
+		Password:    Password,         // Redis账号
+		DB:          0,                // Redis库
+		PoolSize:    Active,           // Redis连接池大小
+		MaxRetries:  Idle,             // 最大重试次数
+		IdleTimeout: 10 * time.Second, // 空闲链接超时时间
+	})
+	pong, err := client.Ping().Result()
+	if err != nil {
+		panic(err)
 	}
+	fmt.Println("redis初始化成功", pong)
+	return client
 }
 
-func GetInstance() *Redis {
-	return &Redis{}
+func GetInstance() *RedisSingleton {
+	once.Do(func() {
+		redisSingleton = &RedisSingleton{client}
+	})
+	return redisSingleton
 }
 
-func (m *Redis) GetConn() *redis.Pool {
-	return m.conn
+func (r *RedisSingleton) GetConn() *redis.Client {
+	return r.client
 }
